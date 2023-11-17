9ec1c26a07310b50fcfaaec48034f8548448892d
matejkostov <matej4o9@gmail.com>
2023-11-17 10:27:20 +0200

    work base design + home responsive buttons

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -2,9 +2,20 @@ import withTopNavigation from "../HOCs/withTopNavigation";
 
 const Work = () => {
   return (
-    <div className="page-wrapper">
-      <div className="content-wrapper">
-        <h1 className="paragraph-title">Work</h1>
+    <div className="page-wrapper mt-10">
+      <div className="content-wrapper gap-10">
+        <div className="move-right-on-hover cursor-pointer flex flex-col gap-2">
+          <h1 className="text-6xl">Netflix Clone</h1>
+          <p className="text-lg text-green-300">Work in Progress</p>
+        </div>
+        <div className="move-right-on-hover cursor-pointer flex flex-col gap-2">
+          <h1 className="text-6xl">To-Do App</h1>
+          <p className="text-lg text-green-300">Work in Progress</p>
+        </div>
+        <div className="move-right-on-hover cursor-pointer flex flex-col gap-2">
+          <h1 className="text-6xl">Perfect Score</h1>
+          <p className="text-lg text-green-300">Work in Progress</p>
+        </div>
       </div>
     </div>
   );
