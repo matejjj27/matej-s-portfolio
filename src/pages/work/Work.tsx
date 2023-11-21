6c92c32aba2042bdff5b6d169504f98dc0becbdc
matejkostov <matej4o9@gmail.com>
2023-11-21 23:44:41 +0200

    make work page responsive

--- src/pages/work/Work.tsx
+++ src/pages/work/Work.tsx
@@ -2,19 +2,19 @@ import withTopNavigation from "../../HOCs/withTopNavigation";
 
 const Work = () => {
   return (
-    <div className="page-wrapper mt-10">
-      <div className="content-wrapper gap-10">
+    <div className="page-wrapper mt-12 max-lg:mt-10 max-sm:mt-6">
+      <div className="content-wrapper gap-14">
         <div className="move-right-on-hover cursor-pointer flex flex-col gap-2">
-          <h1 className="text-6xl">clone app</h1>
-          <p className="text-lg text-green-300">work in progress</p>
+          <h1 className="project-title">clone app</h1>
+          <p className="project-subtitle">work in progress</p>
         </div>
         <div className="move-right-on-hover cursor-pointer flex flex-col gap-2">
-          <h1 className="text-6xl">todo app</h1>
-          <p className="text-lg text-green-300">work in progress</p>
+          <h1 className="project-title">todo app</h1>
+          <p className="project-subtitle">work in progress</p>
         </div>
         <div className="move-right-on-hover cursor-pointer flex flex-col gap-2">
-          <h1 className="text-6xl">perfect score</h1>
-          <p className="text-lg text-green-300">work in progress</p>
+          <h1 className="project-title">perfect score</h1>
+          <p className="project-subtitle">work in progress</p>
         </div>
       </div>
     </div>
