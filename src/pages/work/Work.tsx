747428bed91b8d2549cebd3f50532eb8d5103adc
matejkostov <matej4o9@gmail.com>
2023-11-21 23:59:44 +0200

    update progress to work page

--- src/pages/work/Work.tsx
+++ src/pages/work/Work.tsx
@@ -4,17 +4,19 @@ const Work = () => {
   return (
     <div className="page-wrapper mt-12 max-lg:mt-10 max-sm:mt-6">
       <div className="content-wrapper gap-14">
-        <div className="move-right-on-hover cursor-pointer flex flex-col gap-2">
-          <h1 className="project-title">clone app</h1>
-          <p className="project-subtitle">work in progress</p>
+        <div className="project-wrapper">
+          <h1 className="project-title">portfolio</h1>
+          <p className="project-subtitle">portfolio web app with react</p>
         </div>
-        <div className="move-right-on-hover cursor-pointer flex flex-col gap-2">
-          <h1 className="project-title">todo app</h1>
-          <p className="project-subtitle">work in progress</p>
+        <div className="project-wrapper">
+          <h1 className="project-title">to-doer</h1>
+          <p className="project-subtitle">to-do web app with react</p>
+          <p className="project-subtitle text-orange-300">in progress</p>
         </div>
-        <div className="move-right-on-hover cursor-pointer flex flex-col gap-2">
-          <h1 className="project-title">perfect score</h1>
-          <p className="project-subtitle">work in progress</p>
+        <div className="project-wrapper">
+          <h1 className="project-title">netflix clone</h1>
+          <p className="project-subtitle">web app in react</p>
+          <p className="project-subtitle text-red-400">todo</p>
         </div>
       </div>
     </div>
